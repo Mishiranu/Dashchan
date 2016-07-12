4d60f1c433d7203af04187dbaa6076e7e344d787
Mishiranu <fukurou.mishiranu@gmail.com>
2016-07-12 16:29:59 +0300

    Drawer toggle refactoring

--- src/com/mishiranu/dashchan/util/DrawerToggle.java
+++ src/com/mishiranu/dashchan/util/DrawerToggle.java
@@ -30,14 +30,12 @@ import android.content.Context;
 import android.content.res.Configuration;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.ColorFilter;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PixelFormat;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
-import android.graphics.drawable.InsetDrawable;
 import android.os.Build;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.ViewDragHelper;
@@ -48,19 +46,14 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.mishiranu.dashchan.C;
-import com.mishiranu.dashchan.R;
 
 public class DrawerToggle implements DrawerLayout.DrawerListener
 {
 	private final Activity mActivity;
 	private final DrawerLayout mDrawerLayout;
 	
-	// 5.x arrow
-	private final ArrowDrawable mArrow;
-	
-	// 4.x slider
-	private final SlideDrawable mSlide;
-	private final Drawable mDrawerImage;
+	private final ArrowDrawable mArrowDrawable;
+	private final SlideDrawable mSlideDrawable;
 	private Drawable mHomeAsUpIndicator;
 	
 	public static final int MODE_DISABLED = 0;
@@ -69,23 +62,20 @@ public class DrawerToggle implements DrawerLayout.DrawerListener
 	
 	private int mMode = MODE_DISABLED;
 	
-	@SuppressWarnings("deprecation")
 	public DrawerToggle(Activity activity, DrawerLayout drawerLayout)
 	{
 		mActivity = activity;
 		mDrawerLayout = drawerLayout;
 		if (C.API_LOLLIPOP)
 		{
-			mArrow = new ArrowDrawable(activity.getActionBar().getThemedContext());
-			mDrawerImage = null;
-			mSlide = null;
+			mArrowDrawable = new ArrowDrawable(activity);
+			mSlideDrawable = null;
 		}
 		else
 		{
-			mArrow = null;
+			mArrowDrawable = null;
 			mHomeAsUpIndicator = getThemeUpIndicatorObsolete();
-			mDrawerImage = activity.getResources().getDrawable(R.drawable.ic_drawer);
-			mSlide = new SlideDrawable(mDrawerImage);
+			mSlideDrawable = new SlideDrawable(activity);
 		}
 	}
 	
@@ -124,8 +114,8 @@ public class DrawerToggle implements DrawerLayout.DrawerListener
 				actionBar.setDisplayHomeAsUpEnabled(true);
 				if (C.API_LOLLIPOP)
 				{
-					mActivity.getActionBar().setHomeAsUpIndicator(mArrow);
-					boolean open = mDrawerLayout.isDrawerOpen(Gravity.START) && mArrow.mPosition == 1f;
+					mActivity.getActionBar().setHomeAsUpIndicator(mArrowDrawable);
+					boolean open = mDrawerLayout.isDrawerOpen(Gravity.START) && mArrowDrawable.mPosition == 1f;
 					if (!open)
 					{
 						ValueAnimator animator = ValueAnimator.ofFloat(0f, 1f);
@@ -134,7 +124,7 @@ public class DrawerToggle implements DrawerLayout.DrawerListener
 						animator.start();
 					}
 				}
-				else setActionBarUpIndicatorObsolete(mode == MODE_DRAWER ? mSlide : mHomeAsUpIndicator);
+				else setActionBarUpIndicatorObsolete(mode == MODE_DRAWER ? mSlideDrawable : mHomeAsUpIndicator);
 			}
 		}
 	}
@@ -146,13 +136,13 @@ public class DrawerToggle implements DrawerLayout.DrawerListener
 		{
 			if (C.API_LOLLIPOP)
 			{
-				mArrow.setPosition(mMode == MODE_UP || mDrawerLayout.isDrawerOpen(Gravity.START) ? 1f : 0f);
-				mActivity.getActionBar().setHomeAsUpIndicator(mArrow);
+				mArrowDrawable.setPosition(mMode == MODE_UP || mDrawerLayout.isDrawerOpen(Gravity.START) ? 1f : 0f);
+				mActivity.getActionBar().setHomeAsUpIndicator(mArrowDrawable);
 			}
 			else
 			{
-				mSlide.setPosition(mDrawerLayout.isDrawerOpen(Gravity.START) ? 1f : 0f);
-				setActionBarUpIndicatorObsolete(mMode == MODE_DRAWER ? mSlide : mHomeAsUpIndicator);
+				mSlideDrawable.setPosition(mDrawerLayout.isDrawerOpen(Gravity.START) ? 1f : 0f);
+				setActionBarUpIndicatorObsolete(mMode == MODE_DRAWER ? mSlideDrawable : mHomeAsUpIndicator);
 			}
 		}
 	}
@@ -197,14 +187,14 @@ public class DrawerToggle implements DrawerLayout.DrawerListener
 	{
 		if (C.API_LOLLIPOP)
 		{
-			if (mMode == MODE_DRAWER) mArrow.setPosition(slideOffset);
+			if (mMode == MODE_DRAWER) mArrowDrawable.setPosition(slideOffset);
 		}
 		else
 		{
-			float glyphOffset = mSlide.getPosition();
+			float glyphOffset = mSlideDrawable.getPosition();
 			if (slideOffset > 0.5f) glyphOffset = Math.max(glyphOffset, Math.max(0.f, slideOffset - 0.5f) * 2);
 			else glyphOffset = Math.min(glyphOffset, slideOffset * 2);
-			mSlide.setPosition(glyphOffset);
+			mSlideDrawable.setPosition(glyphOffset);
 		}
 	}
 	
@@ -213,12 +203,9 @@ public class DrawerToggle implements DrawerLayout.DrawerListener
 	{
 		if (C.API_LOLLIPOP)
 		{
-			if (mMode == MODE_DRAWER) mArrow.setPosition(1f);
+			if (mMode == MODE_DRAWER) mArrowDrawable.setPosition(1f);
 		}
-		else
-		{
-			mSlide.setPosition(1);
-		}
+		else mSlideDrawable.setPosition(1);
 	}
 	
 	@Override
@@ -226,12 +213,9 @@ public class DrawerToggle implements DrawerLayout.DrawerListener
 	{
 		if (C.API_LOLLIPOP)
 		{
-			if (mMode == MODE_DRAWER) mArrow.setPosition(0f);
+			if (mMode == MODE_DRAWER) mArrowDrawable.setPosition(0f);
 		}
-		else
-		{
-			mSlide.setPosition(0);
-		}
+		else mSlideDrawable.setPosition(0);
 	}
 	
 	@Override
@@ -244,7 +228,7 @@ public class DrawerToggle implements DrawerLayout.DrawerListener
 	
 	private class ArrowDrawable extends Drawable
 	{
-		private final Paint mPaint = new Paint();
+		private final Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
 		private final Path mPath = new Path();
 		
 		private final float mBarThickness;
@@ -260,10 +244,7 @@ public class DrawerToggle implements DrawerLayout.DrawerListener
 		public ArrowDrawable(Context context)
 		{
 			mPaint.setAntiAlias(true);
-			TypedArray typedArray = context.obtainStyledAttributes(new int[] {android.R.attr.textColorPrimary});
-			int color = typedArray.getColor(0, Color.WHITE);
-			typedArray.recycle();
-			mPaint.setColor(color);
+			mPaint.setColor(0xffffffff);
 			float density = ResourceUtils.obtainDensity(context);
 			mSize = (int) (24f * density);
 			mBarSize = 16f * density;
@@ -287,16 +268,28 @@ public class DrawerToggle implements DrawerLayout.DrawerListener
 		}
 		
 		@Override
+		public int getIntrinsicWidth()
+		{
+			return mSize;
+		}
+		
+		@Override
+		public int getIntrinsicHeight()
+		{
+			return mSize;
+		}
+		
+		@Override
 		public void draw(Canvas canvas)
 		{
 			Rect bounds = getBounds();
-			boolean isLayoutRtl = isLayoutRtl();
+			boolean rtl = isLayoutRtl();
 			float position = mPosition;
 			float arrowSize = AnimationUtils.lerp(mBarSize, mTopBottomArrowSize, position);
 			float middleBarSize = AnimationUtils.lerp(mBarSize, mMiddleArrowSize, position);
 			float middleBarCut = AnimationUtils.lerp(0f, mBarThickness / 2f, position);
 			float rotation = AnimationUtils.lerp(0f, ARROW_HEAD_ANGLE, position);
-			float canvasRotate = AnimationUtils.lerp(isLayoutRtl ? 0f : -180f, isLayoutRtl ? 180f : 0f, position);
+			float canvasRotate = AnimationUtils.lerp(rtl ? 0f : -180f, rtl ? 180f : 0f, position);
 			float topBottomBarOffset = AnimationUtils.lerp(mBarGap + mBarThickness, 0f, position);
 			mPath.rewind();
 			float arrowEdge = -middleBarSize / 2f + 0.5f;
@@ -311,40 +304,22 @@ public class DrawerToggle implements DrawerLayout.DrawerListener
 			mPath.moveTo(0f, 0f);
 			mPath.close();
 			canvas.save();
-			canvas.rotate(canvasRotate * ((mVerticalMirror ^ isLayoutRtl) ? -1f : 1f), bounds.centerX(),
-					bounds.centerY());
+			canvas.rotate(canvasRotate * ((mVerticalMirror ^ rtl) ? -1f : 1f), bounds.centerX(), bounds.centerY());
 			canvas.translate(bounds.centerX(), bounds.centerY());
 			canvas.drawPath(mPath, mPaint);
 			canvas.restore();
 		}
 		
 		@Override
-		public void setAlpha(int i)
+		public void setAlpha(int alpha)
 		{
-			mPaint.setAlpha(i);
-		}
-		
-		public boolean isAutoMirrored()
-		{
-			return true;
+			
 		}
 		
 		@Override
 		public void setColorFilter(ColorFilter colorFilter)
 		{
-			mPaint.setColorFilter(colorFilter);
-		}
-		
-		@Override
-		public int getIntrinsicHeight()
-		{
-			return mSize;
-		}
-		
-		@Override
-		public int getIntrinsicWidth()
-		{
-			return mSize;
+			
 		}
 		
 		@Override
@@ -367,7 +342,83 @@ public class DrawerToggle implements DrawerLayout.DrawerListener
 		public void onAnimationUpdate(ValueAnimator animation)
 		{
 			float value = (float) animation.getAnimatedValue();
-			mArrow.setPosition(mEnable ? 1f - value : value);
+			mArrowDrawable.setPosition(mEnable ? 1f - value : value);
+		}
+	}
+	
+	private class SlideDrawable extends Drawable
+	{
+		private final Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+		private final int mSize;
+		
+		private float mPosition;
+		
+		private SlideDrawable(Context context)
+		{
+			mPaint.setColor(0xff979797);
+			float density = ResourceUtils.obtainDensity(context);
+			mSize = (int) (16f * density);
+		}
+		
+		public void setPosition(float position)
+		{
+			mPosition = position;
+			invalidateSelf();
+		}
+		
+		public float getPosition()
+		{
+			return mPosition;
+		}
+		
+		@Override
+		public int getIntrinsicWidth()
+		{
+			return mSize;
+		}
+		
+		@Override
+		public int getIntrinsicHeight()
+		{
+			return mSize;
+		}
+		
+		@Override
+		public void draw(Canvas canvas)
+		{
+			Rect bounds = getBounds();
+			canvas.save();
+			canvas.translate(bounds.left, bounds.top);
+			boolean rtl = isLayoutRtl();
+			if (rtl)
+			{
+				canvas.translate(bounds.width(), 0);
+				canvas.scale(-1, 1);
+			}
+			canvas.scale(bounds.width() / 48f, bounds.height() / 48f);
+			canvas.translate(-16f * mPosition, 0);
+			canvas.drawRect(0, 4, 30, 12, mPaint);
+			canvas.drawRect(0, 22, 30, 30, mPaint);
+			canvas.drawRect(0, 40, 30, 48, mPaint);
+			canvas.restore();
+		}
+		
+		@Override
+		public void setAlpha(int alpha)
+		{
+			
+		}
+		
+		@Override
+		public void setColorFilter(ColorFilter colorFilter)
+		{
+			
+		}
+		
+		@Override
+		public int getOpacity()
+		{
+			return PixelFormat.TRANSLUCENT;
 		}
 	}
 	
@@ -416,49 +467,6 @@ public class DrawerToggle implements DrawerLayout.DrawerListener
 				if (up instanceof ImageView) mUpIndicatorView = (ImageView) up;
 			}
 			if (mUpIndicatorView != null) mUpIndicatorView.setImageDrawable(upDrawable);
-		}
-	}
-	
-	private class SlideDrawable extends InsetDrawable implements Drawable.Callback
-	{
-		private static final float TOGGLE_DRAWABLE_OFFSET = 1 / 3f;
-		
-		private final Rect mTmpRect = new Rect();
-		
-		private float mPosition;
-		
-		private SlideDrawable(Drawable wrapped)
-		{
-			super(wrapped, 0);
-		}
-		
-		public void setPosition(float position)
-		{
-			mPosition = position;
-			invalidateSelf();
-		}
-		
-		public float getPosition()
-		{
-			return mPosition;
-		}
-		
-		@Override
-		public void draw(Canvas canvas)
-		{
-			copyBounds(mTmpRect);
-			canvas.save();
-			boolean isLayoutRtl = isLayoutRtl();
-			int flipRtl = isLayoutRtl ? -1 : 1;
-			int width = mTmpRect.width();
-			canvas.translate(-TOGGLE_DRAWABLE_OFFSET * width * mPosition * flipRtl, 0);
-			if (isLayoutRtl && !C.API_JELLY_BEAN_MR2)
-			{
-				canvas.translate(width, 0);
-				canvas.scale(-1, 1);
-			}
-			super.draw(canvas);
-			canvas.restore();
 		}
 	}
 	
