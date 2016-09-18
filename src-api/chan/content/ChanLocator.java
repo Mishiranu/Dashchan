c728a71a3329624d1e901080c6ffb3faed6a63af
Mishiranu <fukurou.mishiranu@gmail.com>
2016-09-18 21:15:11 +0300

    Improve YouTube URI pattern

--- src-api/chan/content/ChanLocator.java
+++ src-api/chan/content/ChanLocator.java
@@ -469,7 +469,7 @@ public class ChanLocator implements ChanManager.Linked
 	}
 	
 	private static final Pattern YOUTUBE_URI = Pattern.compile("(?:https?://)(?:www\\.)?(?:m\\.)?" +
-			"youtu(?:\\.be/|be\\.com/(?:v/|(?:#/)?watch\\?(?:.*?|)v=))([\\w\\-]{11})");
+			"youtu(?:\\.be/|be\\.com/(?:v/|embed/|(?:#/)?watch\\?(?:.*?|)v=))([\\w\\-]{11})");
 	
 	private static final Pattern VIMEO_URI = Pattern.compile("(?:https?://)(?:player\\.)?vimeo.com/(?:video/)?" +
 			"(?:channels/staffpicks/)?(\\d+)");
