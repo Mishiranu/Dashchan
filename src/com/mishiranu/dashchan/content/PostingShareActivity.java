be236e4365b4dca1c2ff38a7db16669567f08cd3
Mishiranu <fukurou.mishiranu@gmail.com>
2017-02-09 12:40:35 +0300

    Parse links from text in share activity and redirect to URI handler

--- src/com/mishiranu/dashchan/content/PostingShareActivity.java
+++ src/com/mishiranu/dashchan/content/PostingShareActivity.java
@@ -11,23 +11,39 @@ import com.mishiranu.dashchan.content.storage.DraftsStorage;
 import com.mishiranu.dashchan.util.ToastUtils;
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import chan.util.StringUtils;
 
 public class PostingShareActivity extends Activity {
+	private static final Pattern PATTERN_HREF = Pattern.compile("<a .*href=(\"|')(.*?)\\1.*>");
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		DraftsStorage draftsStorage = DraftsStorage.getInstance();
 		ArrayList<Uri> uris = null;
+		Uri contentUri = null;
 		Intent intent = getIntent();
+
 		if (Intent.ACTION_SEND.equals(intent.getAction())) {
 			Uri uri = intent.getParcelableExtra(Intent.EXTRA_STREAM);
 			if (uri != null) {
 				uris = new ArrayList<>(1);
 				uris.add(uri);
+			} else {
+				String text = StringUtils.emptyIfNull(intent.getStringExtra(Intent.EXTRA_SUBJECT)) + '\n'
+						+ StringUtils.emptyIfNull(intent.getStringExtra(Intent.EXTRA_TEXT));
+				Matcher matcher = PATTERN_HREF.matcher(StringUtils.linkify(text));
+				if (matcher.find()) {
+					contentUri = Uri.parse(matcher.group(2));
+				}
 			}
 		} else if (Intent.ACTION_SEND_MULTIPLE.equals(intent.getAction())) {
 			uris = intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
 		}
+
 		int success = 0;
 		if (uris != null && !uris.isEmpty()) {
 			for (Uri uri : uris) {
@@ -38,7 +54,13 @@ public class PostingShareActivity extends Activity {
 			}
 		}
 
-		ToastUtils.show(this, success > 0 ? R.string.message_draft_saved : R.string.message_unknown_address);
+		if (success > 0) {
+			ToastUtils.show(this, R.string.message_draft_saved);
+		} else if (contentUri != null) {
+			startActivity(new Intent(this, UriHandlerActivity.class).setData(contentUri));
+		} else {
+			ToastUtils.show(this, R.string.message_unknown_address);
+		}
 		finish();
 	}
 }
