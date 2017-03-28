Mishiranu/Dashchan

commit 35f2b1a1fd453431f793db6b140b9fb065464eae

Minor fixes

src/com/mishiranu/dashchan/ui/ActionMenuConfigurator.java
@@ -21,7 +21,7 @@ import java.lang.reflect.Method;
 
 import android.content.Context;
 import android.content.res.Configuration;
-import android.util.SparseArray;
+import android.util.SparseBooleanArray;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -65,7 +65,7 @@ public class ActionMenuConfigurator
 	}
 
 	// True for SHOW_AS_ACTION_ALWAYS, false for SHOW_AS_ACTION_IF_ROOM
-	private final SparseArray<Boolean> mDisplay = new SparseArray<>();
+	private final SparseBooleanArray mDisplay = new SparseBooleanArray();
 	private Menu mLastMenu;
 
 	public void onConfigurationChanged(Configuration newConfig)
@@ -120,12 +120,8 @@ public class ActionMenuConfigurator
 			MenuItem menuItem = menu.getItem(i);
 			if (menuItem.isVisible())
 			{
-				Boolean display = mDisplay.get(menuItem.getItemId());
-				if (display != null)
-				{
-					if (display) used++;
-				}
-				else if (mayOverflow)
+				if (mDisplay.get(menuItem.getItemId())) used++;
+				else if (mayOverflow && mDisplay.indexOfKey(menuItem.getItemId()) < 0)
 				{
 					mayOverflow = false;
 					used++;
@@ -137,8 +133,7 @@ public class ActionMenuConfigurator
 			MenuItem menuItem = menu.getItem(i);
 			if (menuItem.isVisible())
 			{
-				Boolean display = mDisplay.get(menuItem.getItemId());
-				if (display != null && !display)
+				if (mDisplay.get(menuItem.getItemId()))
 				{
 					if (used < maxCount)
 					{

src/com/mishiranu/dashchan/util/ToastUtils.java
@@ -22,6 +22,7 @@ import android.os.Handler;
 import android.os.Looper;
 import android.widget.Toast;
 
+import com.mishiranu.dashchan.R;
 import com.mishiranu.dashchan.content.model.ErrorItem;
 import com.mishiranu.dashchan.widget.ClickableToast;
 
@@ -80,12 +81,12 @@ public class ToastUtils implements Runnable
 
 	public static void show(Context context, int resId)
 	{
-		show(context, context.getString(resId));
+		show(context, context.getString(resId != 0 ? resId : R.string.message_unknown_error));
 	}
 
 	public static void show(Context context, ErrorItem errorItem)
 	{
-		show(context, errorItem.toString());
+		if (errorItem != null) show(context, errorItem.toString()); else show(context, 0);
 	}
 
 	public static void cancel()

commit 067c17d3e67b121f99b8b6e36b5711a66b4cce06

Bug fix: display action bars icons depending on orientation properly (caused by 35f2b1a1fd453431f793db6b140b9fb065464eae)

src/com/mishiranu/dashchan/ui/ActionMenuConfigurator.java
@@ -21,17 +21,21 @@ import java.lang.reflect.Method;
 
 import android.content.Context;
 import android.content.res.Configuration;
-import android.util.SparseBooleanArray;
+import android.util.SparseArray;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.mishiranu.dashchan.content.MainApplication;
+import com.mishiranu.dashchan.util.FlagUtils;
+import com.mishiranu.dashchan.util.Log;
 
 public class ActionMenuConfigurator {
 	private static final Field SHOW_AS_ACTION_FIELD;
 	private static final Object ACTION_BAR_POLICY;
 	private static final Method GET_MAX_ACTION_BUTTONS_METHOD;
 	private static final Method SHOWS_OVERFLOW_MENU_BUTTON_METHOD;
+
+	private enum ShowAsAction {NEVER, ALWAYS, IF_ROOM}
 
 	static {
 		Field showAsActionField;
@@ -48,6 +52,7 @@ public class ActionMenuConfigurator {
 			getMaxActionButtonsMethod = actionBarPolicyClass.getMethod("getMaxActionButtons");
 			showsOverflowMenuButtonMethod = actionBarPolicyClass.getMethod("showsOverflowMenuButton");
 		} catch (Exception e) {
+			Log.persistent().write(e);
 			showAsActionField = null;
 			actionBarPolicy = null;
 			getMaxActionButtonsMethod = null;
@@ -59,8 +64,7 @@ public class ActionMenuConfigurator {
 		SHOWS_OVERFLOW_MENU_BUTTON_METHOD = showsOverflowMenuButtonMethod;
 	}
 
-	// True for SHOW_AS_ACTION_ALWAYS, false for SHOW_AS_ACTION_IF_ROOM
-	private final SparseBooleanArray display = new SparseBooleanArray();
+	private final SparseArray<ShowAsAction> showAsAction = new SparseArray<>();
 	private Menu lastMenu;
 
 	public void onConfigurationChanged(Configuration newConfig) {
@@ -70,21 +74,16 @@ public class ActionMenuConfigurator {
 	}
 
 	public void onAfterCreateOptionsMenu(Menu menu) {
-		display.clear();
+		showAsAction.clear();
 		lastMenu = menu;
 		for (int i = 0; i < menu.size(); i++) {
 			MenuItem menuItem = menu.getItem(i);
 			int id = menuItem.getItemId();
 			int showAsAction = getShowAsAction(menuItem);
-			switch (showAsAction) {
-				case MenuItem.SHOW_AS_ACTION_ALWAYS: {
-					display.put(id, true);
-					break;
-				}
-				case MenuItem.SHOW_AS_ACTION_IF_ROOM: {
-					display.put(id, false);
-					break;
-				}
+			if (FlagUtils.get(showAsAction, MenuItem.SHOW_AS_ACTION_ALWAYS)) {
+				this.showAsAction.put(id, ShowAsAction.ALWAYS);
+			} else if (FlagUtils.get(showAsAction, MenuItem.SHOW_AS_ACTION_IF_ROOM)) {
+				this.showAsAction.put(id, ShowAsAction.IF_ROOM);
 			}
 		}
 	}
@@ -99,21 +98,26 @@ public class ActionMenuConfigurator {
 			return;
 		}
 		int used = 0;
+		// Count displayed icons with flag SHOW_AS_ACTION_ALWAYS
 		for (int i = 0; i < menu.size(); i++) {
 			MenuItem menuItem = menu.getItem(i);
 			if (menuItem.isVisible()) {
-				if (display.get(menuItem.getItemId())) {
+				ShowAsAction showAsAction = this.showAsAction.get(menuItem.getItemId(), ShowAsAction.NEVER);
+				if (showAsAction == ShowAsAction.ALWAYS) {
 					used++;
-				} else if (mayOverflow && display.indexOfKey(menuItem.getItemId()) < 0) {
+				} else if (mayOverflow && showAsAction == ShowAsAction.NEVER) {
+					// Overflow icon will take a place as well
 					mayOverflow = false;
 					used++;
 				}
 			}
 		}
+		// Display SHOW_AS_ACTION_IF_ROOM icons if there is a place left for them
 		for (int i = 0; i < menu.size(); i++) {
 			MenuItem menuItem = menu.getItem(i);
 			if (menuItem.isVisible()) {
-				if (display.get(menuItem.getItemId())) {
+				ShowAsAction showAsAction = this.showAsAction.get(menuItem.getItemId(), ShowAsAction.NEVER);
+				if (showAsAction == ShowAsAction.IF_ROOM) {
 					if (used < maxCount) {
 						used++;
 						menuItem.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);