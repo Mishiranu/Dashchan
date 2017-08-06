25f4b50f217df7310ae0ea7c9cb3e4b90f907083
Mishiranu <fukurou.mishiranu@gmail.com>
2017-08-07 00:10:56 +0300

    Avoid null names for MultipartEntity

--- src-api/chan/http/MultipartEntity.java
+++ src-api/chan/http/MultipartEntity.java
@@ -78,6 +78,9 @@ public class MultipartEntity implements RequestEntity {
 	}
 
 	public final void add(String name, Openable openable, OpenableOutputListener listener) {
+		if (name == null) {
+			throw new NullPointerException("Name is null");
+		}
 		parts.add(new OpenablePart(name, openable, listener));
 	}
 
