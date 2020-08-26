c9d4316ffa2307ccee15e2656d77051956aba289
Mishiranu <fukurou.mishiranu@gmail.com>
2020-08-27 00:37:06 +0300

    Use Chromium User-Agent

--- src/com/mishiranu/dashchan/content/net/UserAgentProvider.java
+++ src/com/mishiranu/dashchan/content/net/UserAgentProvider.java
@@ -1,17 +1,19 @@
 package com.mishiranu.dashchan.content.net;
 
 import android.net.Uri;
+import android.util.Base64;
 import chan.content.ChanLocator;
 import chan.http.HttpException;
 import chan.http.HttpHolder;
 import chan.http.HttpRequest;
-import chan.util.StringUtils;
 import com.mishiranu.dashchan.content.Preferences;
 import com.mishiranu.dashchan.content.async.HttpHolderTask;
 import com.mishiranu.dashchan.util.ConcurrentUtils;
+import com.mishiranu.dashchan.util.Log;
+import java.util.HashMap;
 import java.util.Locale;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class UserAgentProvider {
 	private static final UserAgentProvider INSTANCE = new UserAgentProvider();
@@ -22,99 +24,90 @@ public class UserAgentProvider {
 
 	private UserAgentProvider() {
 		String reference = Preferences.getUserAgentReference();
-		Version3 latest = null;
 		if (reference == null) {
-			reference = "firefox:68.0";
+			reference = "chromium:74.0.3729.169";
 		}
-		if (reference.startsWith("firefox:")) {
-			String version = reference.substring(8);
-			String[] splitted = version.split("\\.");
-			if (splitted != null && splitted.length >= 2) {
-				try {
-					latest = new Version3(Integer.parseInt(splitted[0]), Integer.parseInt(splitted[1]),
-							splitted.length >= 3 ? Integer.parseInt(splitted[2]) : null);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-			userAgent = formatFirefoxUserAgent(version);
+		if (reference.startsWith("chromium:")) {
+			String version = reference.substring(9);
+			userAgent = formatChromiumUserAgent(version);
 		}
-		loadFirefoxUserAgentReference(latest);
+		loadChromiumUserAgentReference();
 	}
 
 	private String userAgent = "Mozilla/5.0";
 
-	private String formatFirefoxUserAgent(String version) {
-		return String.format(Locale.US, "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:%s) " +
-				"Gecko/20100101 Firefox/%s", version, version);
+	private String formatChromiumUserAgent(String version) {
+		return String.format(Locale.US, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " +
+				"(KHTML, like Gecko) Chrome/%s Safari/537.36", version);
 	}
 
-	private static final Pattern FIREFOX_RELEASE_LINK = Pattern
-			.compile("<a href=\".*?\">(\\d+)\\.(\\d+)(?:\\.(\\d+))?/</a>");
-
-	private static class Version3 implements Comparable<Version3> {
-		public final int major;
-		public final int minor;
-		public final Integer patch;
-
-		private Version3(int major, int minor, Integer patch) {
-			this.major = major;
-			this.minor = minor;
-			this.patch = patch;
-		}
-
-		@Override
-		public int compareTo(Version3 o) {
-			int result = Integer.compare(major, o.major);
-			if (result != 0) {
-				return result;
-			}
-			result = Integer.compare(minor, o.minor);
-			if (result != 0) {
-				return result;
-			}
-			return Integer.compare(patch != null ? patch : 0, o.patch != null ? o.patch : 0);
-		}
-
-		@Override
-		public String toString() {
-			return patch != null ? major + "." + minor + "." + patch : major + "." + minor;
-		}
-	}
-
-	private void loadFirefoxUserAgentReference(Version3 initialLatest) {
+	private void loadChromiumUserAgentReference() {
 		new HttpHolderTask<Void, Void, String>() {
 			@Override
 			protected String doInBackground(HttpHolder holder, Void... params) {
-				String response;
+				JSONObject object;
 				try {
-					Uri uri = ChanLocator.getDefault().buildPathWithHost("ftp.mozilla.org", "pub/firefox/releases/");
-					response = new HttpRequest(uri, holder).read().getString();
+					Uri uri = ChanLocator.getDefault().buildQueryWithHost("www.googleapis.com",
+							"storage/v1/b/chromium-browser-snapshots/o",
+							"prefix", "Linux_x64/LAST_CHANGE", "fields", "items(metadata)");
+					object = new HttpRequest(uri, holder).read().getJsonObject();
 				} catch (HttpException e) {
 					return null;
 				}
-				Version3 latest = initialLatest;
-				Matcher matcher = FIREFOX_RELEASE_LINK.matcher(response);
-				while (matcher.find()) {
-					String majorString = matcher.group(1);
-					String minorString = matcher.group(2);
-					String patchString = matcher.group(3);
-					int major = Integer.parseInt(majorString);
-					int minor = Integer.parseInt(minorString);
-					Integer patch = StringUtils.isEmpty(patchString) ? null : Integer.parseInt(patchString);
-					Version3 version = new Version3(major, minor, patch);
-					if (latest == null || version.compareTo(latest) > 0) {
-						latest = version;
+				JSONArray array = object != null ? object.optJSONArray("items") : null;
+				object = array != null && array.length() > 0 ? array.optJSONObject(0) : null;
+				object = object != null ? object.optJSONObject("metadata") : null;
+				String commit = object != null ? object.optString("cr-git-commit") : null;
+
+				if (commit != null) {
+					String response;
+					try {
+						Uri uri = ChanLocator.getDefault().buildQueryWithHost("chromium.googlesource.com",
+								"chromium/src/+/" + commit + "/chrome/VERSION", "format", "TEXT");
+						response = new HttpRequest(uri, holder).read().getString();
+					} catch (HttpException e) {
+						return null;
+					}
+					if (response != null) {
+						String versionData;
+						try {
+							versionData = new String(Base64.decode(response, Base64.DEFAULT));
+						} catch (Exception e) {
+							Log.persistent().stack(e);
+							return null;
+						}
+						HashMap<String, Integer> map = new HashMap<>();
+						for (String line : versionData.split("\n")) {
+							int index = line.indexOf('=');
+							if (index >= 0) {
+								String key = line.substring(0, index);
+								String valueString = line.substring(index + 1);
+								try {
+									int value = Integer.parseInt(valueString);
+									map.put(key.toLowerCase(Locale.US), value);
+								} catch (NumberFormatException e) {
+									// Ignore
+								}
+							}
+						}
+						Integer major = map.get("major");
+						Integer minor = map.get("minor");
+						Integer build = map.get("build");
+						Integer patch = map.get("patch");
+						if (major != null && minor != null) {
+							return major + "." + minor + "." + (build != null ? build : 0) + "." +
+									(patch != null ? patch : 0);
+						}
 					}
 				}
-				return latest != null && latest != initialLatest ? latest.toString() : null;
+				return null;
 			}
 
 			@Override
 			protected void onPostExecute(String version) {
 				if (version != null) {
-					Preferences.setUserAgentReference("firefox:" + version);
-					userAgent = formatFirefoxUserAgent(version);
+					Preferences.setUserAgentReference("chromium:" + version);
+					userAgent = formatChromiumUserAgent(version);
 				}
 			}
 		}.executeOnExecutor(ConcurrentUtils.SEPARATE_EXECUTOR);
