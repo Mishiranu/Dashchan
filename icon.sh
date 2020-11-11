6fd92cb76193b729892fb7e96364999dda30d709
Mishiranu <fukurou.mishiranu@gmail.com>
2020-11-11 09:32:05 +0300

    Update icon.sh and regenerate icons

--- icon.sh
+++ icon.sh
@@ -2,14 +2,16 @@
 
 cp 'icon.svg' 'icon-foreground.svg'
 cp 'icon.svg' 'icon-logo.svg'
-inkscape --select 'dash-group' --verb 'ObjectUnSetClipPath' --verb 'EditDeselect' \
-	--select 'circle' --select 'dash-clip-path' --verb 'EditDelete' --verb 'EditDeselect' \
-	--verb='FileSave' --verb 'FileQuit' 'icon-foreground.svg'
+inkscape --batch-process 'icon-foreground.svg' --actions $(printf '%s;' \
+	'select:dash-group' 'ObjectUnSetClipPath' 'EditDeselect' \
+	'select:circle' 'select:dash-clip-path' 'EditDelete' 'EditDeselect' \
+	'FileSave' 'FileQuit')
 sed -e 's/style=".*"/style="fill:#ffffff"/' -i 'icon-logo.svg'
-inkscape --select 'dash-drop' --verb 'EditDelete' --verb 'EditDeselect' \
-	--select 'dash-group' --verb 'SelectionUnGroup' --verb 'EditDeselect' \
-	--verb 'EditSelectAll' --verb 'SelectionDiff' --verb 'EditDeselect' \
-	--verb='FileSave' --verb 'FileQuit' 'icon-logo.svg'
+inkscape --batch-process 'icon-logo.svg' --actions $(printf '%s;' \
+	'select:dash-drop' 'EditDelete' 'EditDeselect' \
+	'select:dash-group' 'SelectionUnGroup' 'EditDeselect' \
+	'EditSelectAll' 'SelectionDiff' 'EditDeselect' \
+	'FileSave' 'FileQuit')
 
 dimensions=(mdpi:1 hdpi:1.5 xhdpi:2 xxhdpi:3 xxxhdpi:4)
 for dimension in ${dimensions[@]}; do
@@ -20,11 +22,11 @@ for dimension in ${dimensions[@]}; do
 	out_foreground="res/drawable-$resource/ic_launcher_foreground.png"
 	out_logo="res/mipmap-$resource/ic_logo.png"
 	out=("$out_launcher" "$out_foreground" "$out_logo")
-	size="`bc <<< "48 * $scale"`"
-	inkscape 'icon.svg' -a 15:15:93:93 -w "$size" -h "$size" -e "$out_launcher"
-	inkscape 'icon-logo.svg' -a 15:15:93:93 -w "$size" -h "$size" -e "$out_logo"
-	size="`bc <<< "108 * $scale"`"
-	inkscape 'icon-foreground.svg' -w "$size" -h "$size" -e "$out_foreground"
+	size="$(bc <<< "48 * $scale / 1")"
+	inkscape 'icon.svg' --export-area='15:15:93:93' -w "$size" -h "$size" -o "$out_launcher"
+	inkscape 'icon-logo.svg' --export-area='15:15:93:93' -w "$size" -h "$size" -o "$out_logo"
+	size="$(bc <<< "108 * $scale / 1")"
+	inkscape 'icon-foreground.svg' -w "$size" -h "$size" -o "$out_foreground"
 	optipng "${out[@]}"
 	exiftool -all= -overwrite_original "${out[@]}"
 done
